1d8a7713f5ba6b175c818fc35b816092a7e58518
Vladimir Zaitsev <vladimir.zaitsev@lyv.livenation.com>
2023-05-13 22:56:06 +0300

    Added validation for input

--- src/components/formula.svelte
+++ src/components/formula.svelte
@@ -2,10 +2,16 @@
     import { containerStore } from "../stores/containerStore";
     import { elementStore } from "../stores/elementStore";
 
+    const regex : RegExp = /[rhvakces]/i;
+
     let formula: string = '';
 
     function handle(event: any) {
         const input: string = event?.target?.value as string;
+        if (!regex.test(input))
+        {
+            event.target.value = input.slice(0, -1);
+        }
        
         elementStore.updateElements(input);
     }   
