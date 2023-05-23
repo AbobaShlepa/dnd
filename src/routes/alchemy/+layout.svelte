bfa8a916a300f161442d358f4f04bbab2dcdf32b
Vladimir Zaitsev <nicefsf@icloud.com>
2023-05-24 00:54:24 +0300

    Added routing

---
+++ src/routes/alchemy/+layout.svelte
@@ -0,0 +1,27 @@
+<div class="row bg-dark">
+  <div class="col centered">
+    <h1>Алхимическая лаборатория Котофойника</h1>
+  </div>
+</div>
+<div class="row">
+
+</div>
+
+<nav class="navbar bg-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a href="/alchemy">Главная</a>
+    </li>
+    <li class="nav-item">
+      <a href="/alchemy/ingredients">Ингредиенты</a>
+    </li>
+  </ul>
+</nav>
+
+<slot />
+
+<style>
+  .centered {
+    text-align: center;
+  }
+</style>
